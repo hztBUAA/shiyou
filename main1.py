db59b903fc9b9dda246908a721cf9cd828bdb93b
黄振庭 <21371024@buaa.edu.cn>
2024-01-31 15:14:04 +0800

    delete not-used package

--- main1.py
+++ main1.py
@@ -1,7 +1,5 @@
 import sys
-import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
